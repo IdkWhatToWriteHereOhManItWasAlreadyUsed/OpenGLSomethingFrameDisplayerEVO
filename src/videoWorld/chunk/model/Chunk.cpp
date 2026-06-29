abafd4820ef521ef73c167d99cf9b79ae41ded54
Dmitry <154634120+IdkWhatToWriteHereOhManItWasAlreadyUsed@users.noreply.github.com>
2026-06-29 15:42:11 +0300

    Update Chunk.cpp

--- src/videoWorld/chunk/model/Chunk.cpp
+++ src/videoWorld/chunk/model/Chunk.cpp
@@ -1,27 +1,4 @@
-﻿#include "Chunk.h"
+#include "Chunk.h"
 
 namespace OpenGLSomethingFrameDisplayerEVO {
-
-Chunk::Chunk()
-{
-}
-
-Chunk::~Chunk()
-{
-}
-
-void Chunk::SetBlock(int x, int z, int y, const Block block)
-{
-    m_blocks[x][z][y] = block;
-}
-
-Block* Chunk::GetBlock(int x, int z, int y) const
-{
-    return x < CHUNK_WIDTH && x >= 0 
-        && y < CHUNK_HEIGHT && y >= 0 
-        && z < CHUNK_LENGTH && z >= 0
-        ? const_cast<Block*>(&m_blocks[x][z][y])
-        : nullptr;
-}
-
 }
