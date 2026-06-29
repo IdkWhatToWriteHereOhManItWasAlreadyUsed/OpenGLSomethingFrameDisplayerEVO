8fc1e2222824486f147df77d7a4d62a13e75defd
Dmitry <154634120+IdkWhatToWriteHereOhManItWasAlreadyUsed@users.noreply.github.com>
2026-06-29 15:41:51 +0300

    исправил досадную руину performance
    спасибо профилирощику

--- src/videoWorld/chunk/model/Chunk.h
+++ src/videoWorld/chunk/model/Chunk.h
@@ -1,30 +1,50 @@
-﻿#pragma once
+#pragma once
 
 #include <array>
 #include <videoWorld/block/Block.h>
 
 namespace OpenGLSomethingFrameDisplayerEVO {
 
-const int CHUNK_WIDTH = 16;
-const int CHUNK_LENGTH = 16;
-const int CHUNK_HEIGHT = 30;
-
-template<typename T>
-using ChunkModelArray = std::array<std::array<std::array<T, CHUNK_HEIGHT>, CHUNK_WIDTH>, CHUNK_LENGTH>;
+constexpr int CHUNK_WIDTH = 16;
+constexpr int CHUNK_LENGTH = 16;
+constexpr int CHUNK_HEIGHT = 30;
+constexpr int CHUNK_TOTAL_BLOCKS = CHUNK_WIDTH * CHUNK_LENGTH * CHUNK_HEIGHT; // 7680
 
 class Chunk
 {
-	ChunkModelArray<Block> m_blocks;
+private:
+    std::array<Block, CHUNK_TOTAL_BLOCKS> m_blocks;
+
+    static constexpr int GetIndex(int x, int z, int y) noexcept {
+        return (x * CHUNK_LENGTH + z) * CHUNK_HEIGHT + y;
+    }
+
 public:
-	Chunk();
-	~Chunk();
-	// координаты чанка в чанках (НЕ в блоках)
-	int x = 0, z = 0;
-	
-	// другие параметры
-	const ChunkModelArray<Block>& GetBlocks() const { return m_blocks;} 
-	void SetBlock(int x, int z, int y, const Block block);
-	Block* GetBlock(int x, int z, int y) const;  
+    int x = 0, z = 0;
+    Chunk() = default;
+    ~Chunk() = default;
+
+    inline Block* GetBlock(int x, int z, int y) noexcept {
+        if (x < 0 || x >= CHUNK_WIDTH || z < 0 || z >= CHUNK_LENGTH || y < 0 || y >= CHUNK_HEIGHT)
+            return nullptr;
+        return &m_blocks[GetIndex(x, z, y)];
+    }
+
+    inline const Block* GetBlock(int x, int z, int y) const noexcept {
+        if (x < 0 || x >= CHUNK_WIDTH || z < 0 || z >= CHUNK_LENGTH || y < 0 || y >= CHUNK_HEIGHT)
+            return nullptr;
+        return &m_blocks[GetIndex(x, z, y)];
+    }
+
+    inline void SetBlock(int x, int z, int y, const Block& block) {
+        if (x >= 0 && x < CHUNK_WIDTH && z >= 0 && z < CHUNK_LENGTH && y >= 0 && y < CHUNK_HEIGHT)
+            m_blocks[GetIndex(x, z, y)] = block;
+    }
+
+    inline const std::array<Block, CHUNK_TOTAL_BLOCKS>& GetBlocks() const noexcept {
+        return m_blocks;
+    }
+
 };
 
 }
